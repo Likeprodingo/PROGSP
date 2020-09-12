d18a7a79c790783195b95706fda7ad5d693df22b
Likeprodingo <42166797+Likeprodingo@users.noreply.github.com>
2020-09-12 15:22:03 +0300

    Task 1, 2 ,3, 4

--- src/main/java/by/shibaev/task4/tcp/TCPClient.java
+++ src/main/java/by/shibaev/task4/tcp/TCPClient.java
@@ -11,8 +11,12 @@ public class TCPClient {
 
     private final static int DEFAULT_PORT = 2525;
     private final static String QUIT_CMD = "QUIT";
+    private final static String GET_CMD = "GET";
+    private final static String ADD_CMD = "ADD";
+    private final static String EDIT_CMD = "EDIT";
 
     public static void main(String[] arg) {
+        String result;
         String clientMessage;
         try (Socket clientSocket = new Socket("127.0.0.1", 2525);
              BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
@@ -22,7 +26,28 @@ public class TCPClient {
                 System.out.println("Enter any string to send to server \n\t('QUIT' − programme terminate)");
                 clientMessage = stdin.readLine();
                 outputStream.writeObject(clientMessage);
-                //logger
+                switch (clientMessage) {
+                    case GET_CMD:
+                        result = (String) inputStream.readObject();
+                        System.out.println(result);
+                        break;
+                    case ADD_CMD:
+                        System.out.println("Enter name: ");
+                        clientMessage = stdin.readLine();
+                        outputStream.writeObject(clientMessage);
+                        result = (String) inputStream.readObject();
+                        System.out.println(result);
+                        break;
+                    case EDIT_CMD:
+                        System.out.println("Enter name: ");
+                        clientMessage = stdin.readLine();
+                        outputStream.writeObject(clientMessage);
+                        System.out.println("Enter name: ");
+                        clientMessage = stdin.readLine();
+                        outputStream.writeObject(clientMessage);
+                        result = (String) inputStream.readObject();
+                        System.out.println(result);
+                }
             } while (!clientMessage.equals(QUIT_CMD));
         } catch (Exception e) {
             //logger
